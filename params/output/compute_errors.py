1ea38372711b3aeb8f3e53ba7030e6934ec5174a
FranGozze <fgozzerino@gmail.com>
2026-06-10 11:13:18 -0300

    add filter and debug mode to compute_errors

--- params/output/compute_errors.py
+++ params/output/compute_errors.py
@@ -4,6 +4,10 @@ from output_utils import parse_text_list, scale_from_matrix, scale_matrix, compu
 
 import argparse
 
+debug = False
+def printDebugInfo(message):
+    if debug:
+        print(message)
 
 def extract_from_file(data):
     A = np.array([
@@ -32,7 +36,7 @@ def extract_from_gt_file(data):
     return s, s_init, A, t
 
 
-def process_folder(gt_path, results_folder, rowNumber=None):
+def process_folder(gt_path, results_folder, rowNumber=None, transl_noise_filter=None, scale_noise_filter=None, yaw_noise_filter=None):
     row5_data = np.loadtxt(gt_path)
     # file_list = parse_text_list(results_folder)
     file_list = [f.name for f in results_folder.iterdir() if f.is_file()]
@@ -63,6 +67,18 @@ def process_folder(gt_path, results_folder, rowNumber=None):
         yaw_noise_magnitude = partitions[4]
         method = partitions[5]
         idx = partitions[6].split(".")[0]
+
+        if transl_noise_filter is not None and float(transl_noise_magnitude) != float(transl_noise_filter):
+            printDebugInfo(f"Skipping file {file_name} due to translational noise filter: {transl_noise_filter}")
+            continue
+
+        if scale_noise_filter is not None and float(scale_noise_magnitude) != float(scale_noise_filter):
+            printDebugInfo(f"Skipping file {file_name} due to scale noise filter: {scale_noise_filter}")
+            continue
+
+        if yaw_noise_filter is not None and float(yaw_noise_magnitude) != float(yaw_noise_filter):
+            printDebugInfo(f"Skipping file {file_name} due to yaw noise filter: {yaw_noise_filter}")
+            continue
 
         curr_file = np.loadtxt(current_path)
         s, Aff, t, tn, yn, sn = extract_from_file(curr_file)
@@ -117,7 +133,7 @@ def process_folder(gt_path, results_folder, rowNumber=None):
             fail_reasons = f" transl_err: {transl_err}" if abs(transl_err) > 0.05 else ""
             fail_reasons += f" angle_err: {angle_err}" if abs(angle_err) > 0.1 else ""
             fail_reasons += f" scale_err: {scale_err}" if abs(scale_err) > 2.5 else ""
-            print(f"Failed registration case: {file_name} ({fail_reasons})")
+            printDebugInfo(f"Failed registration case: {file_name} ({fail_reasons})")
 
         counter += 1
 
@@ -137,23 +153,31 @@ def main():
     parser.add_argument('--gt',  default="20180524-mavic-ugv-soybean-eschikon-row3_AffineGroundTruth.txt", help='Ground truth file')
     parser.add_argument('--results',  default="20180524-mavic-ugv-soybean-eschikon-row3", help='Folder containing result files to process')
     parser.add_argument('-c', '--complete', action="store_true", help="show all the soybean rows")
-    parser.add_argument('-o', '--output', help='name of output file plot')    
+    parser.add_argument('-o', '--output', help='name of output file plot')
+    parser.add_argument('--show_plots', action="store_true", help='TODO: whether to show the plots after processing')
+    parser.add_argument('--save_plots', action="store_true", help='TODO: whether to save the plots after processing')
+    parser.add_argument('-t', '--filter_transl_noise',  default=None, help='filter out cases without translational noise selected')
+    parser.add_argument('-s', '--filter_scale_noise',  default=None, help='filter out cases without scale noise selected')
+    parser.add_argument('-y', '--filter_yaw_noise',  default=None, help='filter out cases without yaw noise selected')
+    parser.add_argument('-d', '--debug', action="store_true", help='enable debug output')
 
     args = parser.parse_args()
+    global debug 
+    debug = args.debug
     root = Path(__file__).resolve().parent
     if args.complete:
         for x in [3,4,5]:
             gt_file = f"20180524-mavic-ugv-soybean-eschikon-row{x}_AffineGroundTruth.txt"
             results_folder = root / f"20180524-mavic-ugv-soybean-eschikon-row{x}"
             print(f"\nProcessing row {x}...")
-            process_folder(gt_file, results_folder, rowNumber=x)
+            process_folder(gt_file, results_folder, rowNumber=x, transl_noise_filter=args.filter_transl_noise, scale_noise_filter=args.filter_scale_noise, yaw_noise_filter=args.filter_yaw_noise)
     else:
         
         rowNumber = args.gt.split("_")[0][-1]  # Extract row number from GT file name
         gt_path = root / args.gt
         results_folder = root / args.results
         print(f"Processing GT: {gt_path} with results from folder: {results_folder}...")
-        process_folder(gt_path, results_folder, rowNumber=rowNumber)
+        process_folder(gt_path, results_folder, rowNumber=rowNumber, transl_noise_filter=args.filter_transl_noise, scale_noise_filter=args.filter_scale_noise, yaw_noise_filter=args.filter_yaw_noise)
         # row5_path = root / "20180524-mavic-ugv-soybean-eschikon-row5_AffineGroundTruth.txt"
         # list_path = root / "file_list.txt"
 
